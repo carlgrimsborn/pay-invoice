3c105d31883da6065bab87f7c99bfff8a8bedc37
carlgrimsborn <carlgrimsborn@hotmail.com>
2022-01-18 01:52:11 +0100

    keyboard aware

--- screens/Invoice.tsx
+++ screens/Invoice.tsx
@@ -1,7 +1,16 @@
 import React, { useState } from "react";
-import { View, Image, StyleSheet, Text, TextInput, Button } from "react-native";
+import {
+  View,
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  Button,
+  ScrollView,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useOvermindState } from "../overmind";
+import { KeyboardAwareView } from "react-native-keyboard-aware-view";
 
 const Invoice = () => {
   const { payment } = useOvermindState();
@@ -9,63 +18,77 @@ const Invoice = () => {
   const [amount, setAmount] = useState("");
   const [dueDate, setDueDate] = useState("");
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.header}>Scanned image: </Text>
-      <Image style={styles.image} source={{ uri: payment.uri }}></Image>
-      <Text style={styles.header2}>Is following the information correct? </Text>
-      <View style={styles.formView}>
-        <View style={styles.formRow}>
-          <Text style={styles.formText}>Reciever: </Text>
-          <TextInput
-            style={styles.input}
-            value={reciever}
-            onChangeText={(t) => setReciever(t)}
-          ></TextInput>
-        </View>
-        <View style={styles.formRow}>
-          <Text style={styles.formText}>Amount: </Text>
-          <TextInput
-            style={styles.input}
-            value={amount}
-            onChangeText={(t) => setAmount(t)}
-          ></TextInput>
-        </View>
-        <View style={styles.formRow}>
-          <Text style={styles.formText}>Due date: </Text>
-          <TextInput
-            style={styles.input}
-            value={dueDate}
-            onChangeText={(t) => setDueDate(t)}
-          ></TextInput>
-        </View>
-      </View>
+    <SafeAreaView style={styles.parentContainer}>
+      <View style={styles.container}>
+        <KeyboardAwareView animated={true}>
+          <ScrollView
+            style={{ flex: 1 }}
+            contentContainerStyle={{ alignItems: "center" }}
+          >
+            <Text style={styles.header}>Scanned image: </Text>
+            <Image style={styles.image} source={{ uri: payment.uri }}></Image>
+            <Text style={styles.header2}>
+              Is following the information correct?{" "}
+            </Text>
 
-      <Button title="Next" onPress={() => {}}></Button>
+            <View>
+              <View style={styles.formRow}>
+                <Text style={styles.formText}>Reciever: </Text>
+                <TextInput
+                  style={styles.input}
+                  value={reciever}
+                  onChangeText={(t) => setReciever(t)}
+                ></TextInput>
+              </View>
+              <View style={styles.formRow}>
+                <Text style={styles.formText}>Amount: </Text>
+                <TextInput
+                  style={styles.input}
+                  value={amount}
+                  onChangeText={(t) => setAmount(t)}
+                ></TextInput>
+              </View>
+              <View style={styles.formRow}>
+                <Text style={styles.formText}>Due date: </Text>
+                <TextInput
+                  style={styles.input}
+                  value={dueDate}
+                  onChangeText={(t) => setDueDate(t)}
+                ></TextInput>
+              </View>
+            </View>
+          </ScrollView>
+        </KeyboardAwareView>
+      </View>
+      <View>
+        <Button title="Next" onPress={() => {}}></Button>
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  parentContainer: {
+    flex: 1,
+    justifyContent: "space-between",
+  },
   container: {
     flex: 1,
     alignItems: "center",
     marginHorizontal: 20,
-    justifyContent: "space-between",
   },
   image: {
     width: "100%",
-    height: 300,
-  },
-  formView: {
-    paddingHorizontal: 80,
-    alignItems: "center",
+    height: 200,
   },
   formRow: {
     flexDirection: "row",
     marginVertical: 20,
+    width: "90%",
+    justifyContent: "space-around",
   },
   input: {
-    width: "100%",
+    flex: 1,
     borderBottomWidth: 1,
     fontSize: 20,
   },
